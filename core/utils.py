b31464400a996b18b085397549d59aff46a55e10
Min ho Kim <minho42@gmail.com>
2020-06-07 00:08:00 +1000

    change get_chromedriver for heroku copied from healthroster

--- core/utils.py
+++ core/utils.py
@@ -33,7 +33,11 @@ def get_chromedriver(headless: bool = True) -> object:
     prefs = {"profile.managed_default_content_settings.images": 2}
     if headless:
         options.add_argument("headless")
-        options.add_argument("--window-size=1920x1080")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--disable-dev-shm-usage")
+        # options.add_argument("--remote-debugging-port=9222")
+        # options.add_argument("--window-size=1920x1080")
     options.add_experimental_option("prefs", prefs)
     # options.add_argument(f'user-agent={USER_AGENT}')
     # options.add_argument("disable-infobars") --> flag removed: https://chromium.googlesource.com/chromium/src/+/d869ab3350d8ebd95222b4a47adf87ce3d3214b1
@@ -43,8 +47,13 @@ def get_chromedriver(headless: bool = True) -> object:
     # options.add_argument("--disable-plugins-discovery")
     # options.add_argument("--start-maximized")
     # options.add_argument("--no-proxy-server")
-    driver = webdriver.Chrome(os.environ.get("CHROME_DRIVER_PATH"), options=options)
+    # driver = webdriver.Chrome(os.environ.get("CHROME_DRIVER_PATH"), options=options)
 
+    options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+
+    driver = webdriver.Chrome(
+        executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=options
+    )
     return driver
 
 
