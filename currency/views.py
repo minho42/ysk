17e9973d81f3ec7753c72afc520af0cee9cc85c0
Min ho Kim <minho42@gmail.com>
2020-06-10 00:09:10 +1000

    removed deleting all for now

--- currency/views.py
+++ currency/views.py
@@ -421,7 +421,7 @@ def get_all():
 @timeit
 def get_new_api_data():
     # deleting all makes modified useless as created_at shares same value...
-    Currency.objects.all().delete()
+    # Currency.objects.all().delete()
 
     currencies = [
         # Using requests
