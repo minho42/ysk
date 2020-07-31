57ef5f1f2f940027af2082a29dfed6f2dc82f6d6
Min ho Kim <minho42@gmail.com>
2020-08-01 01:06:56 +1000

    allowed_hosts for production

--- settings/production.py
+++ settings/production.py
@@ -2,6 +2,12 @@ from .base import *
 
 DEBUG = False
 
+ALLOWED_HOSTS += [
+    "audkrw.com",
+    "www.audkrw.com",
+    "149.28.174.146",
+]
+
 REDIS_URL = get_env_variable("REDIS_URL")
 
 CELERY_BROKER_URL = REDIS_URL
