22399cd3254b29d89ffab5f3b6accb785f043b11
Min ho Kim <minho42@gmail.com>
2021-01-03 00:16:52 +1100

    crontab 15 -> 5 minutes

--- currency/tasks.py
+++ currency/tasks.py
@@ -8,7 +8,7 @@ from .views import get_new_api_data
 
 @app.on_after_finalize.connect
 def setup_periodic_tasks(sender, **kwargs):
-    sender.add_periodic_task(crontab(minute="*/15"), fetch_all.s(), name="")
+    sender.add_periodic_task(crontab(minute="*/5"), fetch_all.s(), name="")
 
 
 @app.task
