c91aff1156d020b6e5f202ca4fcedc9be9cb81a1
Min ho Kim <minho42@gmail.com>
2021-01-03 00:37:57 +1100

    add note

--- currency/models.py
+++ currency/models.py
@@ -8,6 +8,7 @@ class Currency(TimeStampedModel):
     fee = models.DecimalField(max_digits=4, decimal_places=2, default=0, null=True)
     real_rate = models.DecimalField(max_digits=6, decimal_places=2, default=0)
     url = models.CharField(max_length=400)
+    note = models.CharField(max_length=1000, null=True, blank=True)
 
     def __str__(self):
         return self.name
