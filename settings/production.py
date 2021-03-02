8c568900218b6429b536e3d3648549fa4b54151d
Min ho Kim <minho42@gmail.com>
2021-03-03 01:18:51 +1100

    add sentry

--- settings/production.py
+++ settings/production.py
@@ -11,3 +11,19 @@ REDIS_URL = env("REDIS_URL")
 
 CELERY_BROKER_URL = REDIS_URL
 CELERY_RESULT_BACKEND = REDIS_URL
+
+
+# Sentry
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+from sentry_sdk.integrations.celery import CeleryIntegration
+from sentry_sdk.integrations.redis import RedisIntegration
+
+sentry_sdk.init(
+    dsn="https://0ef35c90768b486e89b1e52933eff293@o410328.ingest.sentry.io/5656262",
+    integrations=[DjangoIntegration(), CeleryIntegration(), RedisIntegration()],
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+)
+
--- ysk/urls.py
+++ ysk/urls.py
@@ -10,3 +10,12 @@ urlpatterns = [
     path("about/", views.about, name="about"),
     path("paigeisawesome/", admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
+urlpatterns += [
+    path("sentry-debug/", trigger_error),
+]
