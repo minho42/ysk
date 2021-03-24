76eee44ceb234a6a64019afb3db062157bfec285
Min ho Kim <minho42@gmail.com>
2021-03-24 23:11:32 +1100

    shorter navbar height

--- frontend/components/Navbar.vue
+++ frontend/components/Navbar.vue
@@ -2,7 +2,7 @@
   <nav id="navbar">
     <header
       class="flex justify-center border-b border-gray-500 shadow-sm">
-      <div class="flex items-center justify-between pl-3 py-2 h-16">
+      <div class="flex items-center justify-between pl-3 py-2 h-12">
         <div class="flex items-center">
           <NuxtLink to="/" class="px-1 py-1 font-semibold text-lg text-gray-200 hover:text-gray-300">
             호주 역송금 비교
