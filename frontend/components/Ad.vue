f8f17859dffc2776c8c6cebb652fd60caaa3fa7d
Min ho Kim <minho42@gmail.com>
2021-04-02 09:11:50 +1100

    style

--- frontend/components/Ad.vue
+++ frontend/components/Ad.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="flex items-center justify-center pt-2 pb-2 border-b border-gray-500">
+  <div class="flex items-center justify-center py-4 border-b border-gray-500">
     <div class="">
       <a 
-      class="text-lg text-gray-400 hover:underline tracking-wide"
+      class="text-lg hover:underline tracking-wide"
       href="https://www.coinbase.com/join/kim_dk4p" target="_blank" rel="noopener noreferrer">
         Get free $10 in Bitcoin on Coinbase! 
       </a>
     </div>
-    <div class="text-gray-400 ml-1">
+    <div class="ml-1">
       <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"></path></svg>
     </div>
   </div>  
