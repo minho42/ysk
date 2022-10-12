d763f325b6119277d84cba951aaf4c556cdf5047
Min ho Kim <minho42@gmail.com>
2022-10-13 09:04:01 +1100

    remove azimo: no longer in business

--- currency/views.py
+++ currency/views.py
@@ -538,63 +538,6 @@ def scrape_orbitremit():
     return (rate, fee)
 
 
-def scrape_azimo():
-    # Azimo
-    # Using XHR
-
-    url = f"https://api.azimo.com/service-rates/v1/public/en/prices/current?sendingCountry=AUS&sendingCurrency=AUD&receivingCountry=KOR&receivingCurrency=USD&deliveryMethod=SWIFT"
-    headers = {
-        "authority": "api.azimo.com",
-        "method": "GET",
-        "path": "/service-rates/v1/public/en/prices/current?sendingCountry=AUS&sendingCurrency=AUD&receivingCountry=KOR&receivingCurrency=USD&deliveryMethod=SWIFT",
-        "scheme": "https",
-        "accept": "*/*",
-        "accept-encoding": "gzip, deflate, br",
-        "accept-language": "en-AU,en;q=0.9,ko-KR;q=0.8,ko;q=0.7,en-GB;q=0.6,en-US;q=0.5",
-        "cache-control": "no-cache",
-        "origin": "https://azimo.com",
-        "pragma": "no-cache",
-        "referer": "https://azimo.com/en/send-money-to-korea-republic-of",
-        "sec-ch-ua": '"Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"',
-        "sec-ch-ua-mobile": "?0",
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-site",
-        "user-agent": USER_AGENT,
-        "x-api-version": "3.27.0",
-        "x-app-version": "LEGO-CLIENT,4.49.48",
-        "x-application-calculator": "INDIVIDUAL",
-        "x-azimo-utdm": "10407fcc-1be8-4231-bf15-d3b2da5dd434",
-        "x-correlation-id": "LEGO-CLIENT-51e5fdfa-19ae-4c23-9d0f-6eeab29507d7",
-        "x-platform-version": "2021.12",
-    }
-    s = requests.session()
-    r = s.get(url=url, headers=headers)
-    rr = json.loads(r.text)
-
-    try:
-        fee = rr["rates"][0]["adjustments"]["fee"]["max"]["was"]
-        rate_in_usd = rate = rr["rates"][0]["adjustments"]["rate"]["value"]["was"]
-        # Convert USD -> KRW
-        usd_krw_rate = scrape_naver_usd()
-        if usd_krw_rate:
-            rate = float(rate_in_usd) * float(usd_krw_rate)
-        else:
-            print("Can't convert USD->KRW")
-            return (0.0, 0.0)
-    except (KeyError, IndexError):
-        rate = 0
-        fee = 0
-
-    if rate:
-        # note = "Receive in USD"
-        note = f"USD->KRW: {rate_in_usd} x {usd_krw_rate} = {round_it(rate)}"
-
-        return (rate, fee, note)
-
-    return (rate, fee)
-
-
 def scrape_stra():
     rate = scrape_currency("http://1472.com.au/", '//div[@class="aukr"]/div[@class="ex_bg"]/span')
     # TODO Login to scrape fee
@@ -671,11 +614,6 @@ def save_remitly():
 
 
 @timeit
-def save_azimo():
-    return save_currency("Azimo", "https://azimo.com/en/send-money-to-korea-republic-of", scrape_azimo)
-
-
-@timeit
 def save_orbitremit():
     return save_currency("OrbitRemit", "https://www.orbitremit.com", scrape_orbitremit)
 
@@ -743,8 +681,7 @@ def fetch_new_data():
     save_wirebarley(),
     save_remitly(),
     save_instarem(),
-    save_azimo(),
-    save_orbitremit(),
+    # save_orbitremit(),
     # Using selenium
     save_dondirect(),
     save_wontop(),
